fa1c8a99ff2db1808c8a14cea94c28a137938fb6
Yudai Yamamoto <udai11up@gmail.com>
2018-01-28 20:33:40 +0900

    add WordPress Plugin Template

---
+++ wp-plugin-template/plugin.php
@@ -0,0 +1,28 @@
+<?php
+/*
+    Plugin Name: WP Plugin Template
+    Plugin URI:
+    Description: WordPress Plugin Template
+    Version: 1.0.0
+    Author: Your name
+    Author URI: http://example.com
+    License: GPL2
+*/
+?>
+<?php
+/*  Copyright 2018 your name (email : you@example.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
